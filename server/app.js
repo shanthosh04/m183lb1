9a6ceb94d77413ef834c69a397081c262075a49b
shanthosh04 <133089176+shanthosh04@users.noreply.github.com>
2023-10-25 16:27:31 +0200

    add rate limit

--- server/app.js
+++ server/app.js
@@ -1,25 +1,34 @@
 const express = require("express");
 const http = require("http");
 const { initializeAPI } = require("./api");
+const { rateLimit } = require("express-rate-limit");
 
-// Create the express server
+
 const app = express();
 app.use(express.json());
 const server = http.createServer(app);
 
-// deliver static files from the client folder like css, js, images
+
 app.use(express.static("client"));
-// route for the homepage
+
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/client/index.html");
 });
 
 
+const limiter = rateLimit({
+ windowMs: 60 * 1000, 
+ limit: 50, 
+ standardHeaders: "draft-7",
+ legacyHeaders: false,
+});
 
-// Initialize the REST api
+app.use(limiter);
+
+
 initializeAPI(app);
 
-//start the web server
+
 const serverPort = process.env.PORT || 3000;
 server.listen(serverPort, () => {
   console.log(`Express Server started on port ${serverPort}`);
